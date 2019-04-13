0e87000eeeba0ffcf0845df7c9b9425c6957d3bd
Andrew English <aje299@gmail.com>
2019-04-13 15:37:30 -0700

    Added connection to the MongoDB cloud server.

--- app.js
+++ app.js
@@ -4,12 +4,21 @@
 const Express = require('express'); //Requires the express module (a class)
 const app = Express(); //Instantiates an express object
 
+
+//Sets up standard connection to the database and binds the error event so errors are printed on the console
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://SimpollUser:HADsimpollisgr8@evoting-3ak9s.mongodb.net/Simpoll?retryWrites=true', { useNewUrlParser: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
 //the client will send a get request to the "/" link when first joining 
 // and we respond with "Hello World"
 app.get('/', (request, response) => {
    response.send('Hello World');
-   console.log('User connected!');  
+   console.log('User connected!'); 
 });
+
 
 //This starts the server and displays in the terminal which port the server is listening on
 //Connect by typing "localhost:3000" on web browser
