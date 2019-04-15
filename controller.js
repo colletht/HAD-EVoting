1b2eaf510b21debe925e400dda7231d10050d3ef
Harry Collet <harrison.collet@wsu.edu>
2019-04-15 16:38:25 -0700

    created controller module to process and handle HTTP requests, began thinking about route names and actions

---
+++ controller.js
@@ -0,0 +1,30 @@
+/*
+* Name: Harry Collet
+* Date: 4/15/19
+* Description: Controller module defined here that processes all routing and HTTP requests. 
+*              This object is resposible for processing these requests and calling the 
+*              appropriate database functions and then eachoing the appropriate result back
+*              to the web.
+*/
+
+
+//require necessary modules
+var express = require('express');
+var router = express.Router();
+
+//homepage
+router.get('/', function (req, res){
+    res.send("Homepage");   //temp response just to set up the blueprint for functions
+    //later use res.render() renders HTML from out data
+});
+
+//login page getter
+router.get('/login', function(req, res){
+    res.send("Login Page");     //
+});
+
+router.get('/polls', function(req, res){
+    res.send("There a no polls at the moment: Be the first to create one!");
+});
+
+module.exports = router;
