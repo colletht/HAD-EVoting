1a83a10e48a070ca70b2cb64715d1fdfbd2e1eed
Andrew English <aje299@gmail.com>
2019-04-17 15:01:08 -0700

    Moved DB connection to DB_Interface.js

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 //Basic Hello World Server
 //(Press ctrl^c to stop server running in terminal)
 
+// !! max number of questions and options is 4 !!
+
 const Express = require('express'); //Requires the express module (a class)
 const app = Express(); //Instantiates an express object
-
-
-//Sets up standard connection to the database and binds the error event so errors are printed on the console
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://SimpollUser:HADsimpollisgr8@evoting-3ak9s.mongodb.net/Simpoll?retryWrites=true', { useNewUrlParser: true });
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 
 //the client will send a get request to the "/" link when first joining 
