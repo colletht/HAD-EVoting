d35caf7d695ad158bd50b1db5b22cb8dce3aa573
Andrew English <aje299@gmail.com>
2019-04-18 15:22:13 -0700

    Cleaned up semicolons

--- app.js
+++ app.js
@@ -3,20 +3,20 @@
 
 // !! max number of questions and options is 4 !!
 
-const Express = require('express'); //Requires the express module (a class)
-const app = Express(); //Instantiates an express object
+const Express = require('express') //Requires the express module (a class)
+const app = Express() //Instantiates an express object
 
 
 //the client will send a get request to the "/" link when first joining 
 // and we respond with "Hello World"
 app.get('/', (request, response) => {
-   response.send('Hello World');
-   console.log('User connected!'); 
+   response.send('Hello World')
+   console.log('User connected!') 
 });
 
 
 //This starts the server and displays in the terminal which port the server is listening on
 //Connect by typing "localhost:3000" on web browser
 app.listen(3000, () => {
-   console.log('Listening on port 3000...');
-});
+   console.log('Listening on port 3000...')
+});  
