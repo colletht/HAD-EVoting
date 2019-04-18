6f29bcfdbcefb1e026a54c81c6fa7933e57ec064
Harry Collet <harrison.collet@wsu.edu>
2019-04-18 12:46:48 -0700

    Added definitions for registering and logging in a user processes, with the exeption of actually putting them in the database which will be done later when the database branch is merged back to master

--- controllers/PollController.js
+++ controllers/PollController.js
@@ -7,54 +7,58 @@
 *              to the web.
 */
 
+//require the needed modules for validating and sanitizing form results
+const {body, validationResult} = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
+
 //require model that drew made
 //var poll = require(<path to poll module>)
 
 exports.pollList = function(req,res){
-    res.send("Print out a list of all polls in database");
+    res.send("NOT IMPLEMENTED: Print out a list of all polls in database");
 };
 
 exports.pollDetail = function(req,res){
-    res.send("Print out the data for a specific poll from database");
+    res.send("NOT IMPLEMENTED: Print out the data for a specific poll from database");
 };
 
 exports.pollCreateGet = function(req,res){
-    res.send("Handles the creation from for a poll GET");
+    res.send("NOT IMPLEMENTED: Handles the creation from for a poll GET");
 };
 
 exports.pollCreatePost = function(req,res){
-    res.send("Handles the creation form for a poll POST");
+    res.send("NOT IMPLEMENTED: Handles the creation form for a poll POST");
 };
 
 exports.pollDeleteGet = function(req,res){
-    res.send("Handles the deletion from for a poll GET");
+    res.send("NOT IMPLEMENTED: Handles the deletion from for a poll GET");
 };
 
 exports.pollDeletePost = function(req,res){
-    res.send("Handles the deletion form for a poll POST");
+    res.send("NOT IMPLEMENTED: Handles the deletion form for a poll POST");
 };
 
 exports.pollCompleteGet = function(req,res){
-    res.send("Handles the completion of poll by user GET");
+    res.send("NOT IMPLEMENTED: Handles the completion of poll by user GET");
 };
 
 exports.pollCompletePost = function(req,res){
-    res.send("Handles the completion of the poll by user POST");
+    res.send("NOT IMPLEMENTED: Handles the completion of the poll by user POST");
 };
 
 exports.pollEndGet = function(req,res){
-    res.send("Handles the ending of polling period GET");
+    res.send("NOT IMPLEMENTED: Handles the ending of polling period GET");
 };
 
 exports.pollEndPost = function(req,res){
-    res.send("Handles the ending of polling period POST");
+    res.send("NOT IMPLEMENTED: Handles the ending of polling period POST");
 };
 
 exports.pollShareGet = function(req,res){
-    res.send("Handles the share form for poll GET");
+    res.send("NOT IMPLEMENTED: Handles the share form for poll GET");
 };
 
 exports.pollSharePost = function(req,res){
-    res.send("Handles the share form for a poll POST");
+    res.send("NOT IMPLEMENTED: Handles the share form for a poll POST");
 };
 
