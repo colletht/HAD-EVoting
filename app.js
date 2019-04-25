4245775868a281dae00e5cf0280e39faf4e2d0ae
Harry Collet <harrison.collet@wsu.edu>
2019-04-25 11:40:18 -0700

    making app work

--- app.js
+++ app.js
@@ -32,6 +32,6 @@ app.use('/',routes);
 
 //This starts the server and displays in the terminal which port the server is listening on
 //Connect by typing "localhost:3000" on web browser
-app.listen(3000, () => {
-   console.log('Listening on port 3000...')
+app.listen((process.env.PORT || 3000 ), () => {
+   console.log('Listening ' + process.env.PORT);
 });  
